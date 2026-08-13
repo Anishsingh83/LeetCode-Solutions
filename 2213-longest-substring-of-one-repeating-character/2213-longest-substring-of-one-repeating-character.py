d89f420d605714b7d5e5e6bf863e4f5b7503de34
Anish Singh <anishsingh.70455@gmail.com>
2026-08-13 07:23:58 +0530

    Time: 2624 ms (70.49%), Space: 33.1 MB (100%) - LeetHub

--- 2213-longest-substring-of-one-repeating-character/2213-longest-substring-of-one-repeating-character.py
+++ 2213-longest-substring-of-one-repeating-character/2213-longest-substring-of-one-repeating-character.py
@@ -1,44 +1,57 @@
+from sortedcontainers import SortedList
+
 class Solution:
     def longestRepeating(self, s: str, queryCharacters: str, queryIndices: List[int]) -> List[int]:
         n = len(s)
-        tree = [None] * (4 * n)
         arr = list(s)
+        bounds = SortedList([0] + [i for i in range(1, n) if arr[i] != arr[i-1]])
+        lens = SortedList()
+        for idx, st in enumerate(bounds):
+            en = bounds[idx+1] if idx+1 < len(bounds) else n
+            lens.add(en - st)
 
-        def merge(l, r):
-            if not l: return r
-            if not r: return l
-            lc, lr, lp, ls, lb, ll = l
-            rl, rc, rp, rs, rb, rl_ = r
-            best = max(lb, rb)
-            pre, suf = lp, rs
-            if lr == rl:
-                best = max(best, ls + rp)
-                if lp == ll: pre = ll + rp
-                if rs == rl_: suf = ls + rl_
-            return (lc, rc, pre, suf, best, ll + rl_)
+        def get_run(i):
+            pos = bounds.bisect_right(i) - 1
+            st = bounds[pos]
+            en = bounds[pos+1] if pos+1 < len(bounds) else n
+            return st, en
 
-        def build(node, start, end):
-            if start == end:
-                tree[node] = (arr[start],) * 2 + (1, 1, 1, 1)
-                return
-            mid = (start + end) // 2
-            build(node*2, start, mid)
-            build(node*2+1, mid+1, end)
-            tree[node] = merge(tree[node*2], tree[node*2+1])
-
-        def update(node, start, end, i, c):
-            if start == end:
-                tree[node] = (c, c, 1, 1, 1, 1)
-                return
-            mid = (start + end) // 2
-            update(node*2, start, mid, i, c) if i <= mid else update(node*2+1, mid+1, end, i, c)
-            tree[node] = merge(tree[node*2], tree[node*2+1])
-
-        build(1, 0, n - 1)
         ans = []
         for c, i in zip(queryCharacters, queryIndices):
-            if arr[i] != c:
-                arr[i] = c
-                update(1, 0, n - 1, i, c)
-            ans.append(tree[1][4])
+            if arr[i] == c:
+                ans.append(lens[-1])
+                continue
+
+            st, en = get_run(i)
+            lens.remove(en - st)
+            bounds.remove(st)
+
+            if i > st:
+                bounds.add(st)
+                lens.add(i - st)
+            if i + 1 < en:
+                bounds.add(i + 1)
+                lens.add(en - i - 1)
+            bounds.add(i)
+            lens.add(1)
+            arr[i] = c
+
+            if i + 1 < n and arr[i + 1] == c:
+                st1, en1 = get_run(i)
+                st2, en2 = get_run(i + 1)
+                lens.remove(en1 - st1)
+                lens.remove(en2 - st2)
+                bounds.remove(st2)
+                lens.add(en2 - st1)
+
+            if i > 0 and arr[i - 1] == c:
+                st1, en1 = get_run(i - 1)
+                st2, en2 = get_run(i)
+                lens.remove(en1 - st1)
+                lens.remove(en2 - st2)
+                bounds.remove(st2)
+                lens.add(en2 - st1)
+
+            ans.append(lens[-1])
+
         return ans
