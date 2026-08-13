f98a5da8ba536851211640de65c2146dfc462294
Anish Singh <anishsingh.70455@gmail.com>
2026-08-13 07:17:58 +0530

    Time: 2263 ms (77.05%), Space: 141.6 MB (9.84%) - LeetHub

--- 2213-longest-substring-of-one-repeating-character/2213-longest-substring-of-one-repeating-character.py
+++ 2213-longest-substring-of-one-repeating-character/2213-longest-substring-of-one-repeating-character.py
@@ -1,13 +1,12 @@
 class Solution:
     def longestRepeating(self, s: str, queryCharacters: str, queryIndices: List[int]) -> List[int]:
-
         n = len(s)
         tree = [None] * (4 * n)
+        arr = list(s)
 
         def merge(left, right):
             if left is None:
                 return right
-
             if right is None:
                 return left
 
@@ -17,97 +16,47 @@ class Solution:
             length = l_len + r_len
             prefix = l_pre
             suffix = r_suf
-            best = max(l_best, r_best)
+            best = l_best if l_best > r_best else r_best
 
             if l_right == r_left:
-                best = max(best, l_suf + r_pre)
-
+                mid_run = l_suf + r_pre
+                if mid_run > best:
+                    best = mid_run
                 if l_pre == l_len:
                     prefix = l_len + r_pre
-
                 if r_suf == r_len:
                     suffix = l_suf + r_len
 
-            return [
-                l_char,
-                r_char,
-                prefix,
-                suffix,
-                best,
-                length
-            ]
+            return (l_char, r_char, prefix, suffix, best, length)
 
         def build(node, start, end):
             if start == end:
-                tree[node] = [
-                    s[start],
-                    s[start],
-                    1,
-                    1,
-                    1,
-                    1
-                ]
+                c = arr[start]
+                tree[node] = (c, c, 1, 1, 1, 1)
                 return
-
             mid = (start + end) // 2
-
             build(node * 2, start, mid)
             build(node * 2 + 1, mid + 1, end)
-
-            tree[node] = merge(
-                tree[node * 2],
-                tree[node * 2 + 1]
-            )
+            tree[node] = merge(tree[node * 2], tree[node * 2 + 1])
 
         def update(node, start, end, index, char):
             if start == end:
-                tree[node] = [
-                    char,
-                    char,
-                    1,
-                    1,
-                    1,
-                    1
-                ]
+                tree[node] = (char, char, 1, 1, 1, 1)
                 return
-
             mid = (start + end) // 2
-
             if index <= mid:
-                update(
-                    node * 2,
-                    start,
-                    mid,
-                    index,
-                    char
-                )
+                update(node * 2, start, mid, index, char)
             else:
-                update(
-                    node * 2 + 1,
-                    mid + 1,
-                    end,
-                    index,
-                    char
-                )
-
-            tree[node] = merge(
-                tree[node * 2],
-                tree[node * 2 + 1]
-            )
+                update(node * 2 + 1, mid + 1, end, index, char)
+            tree[node] = merge(tree[node * 2], tree[node * 2 + 1])
 
         build(1, 0, n - 1)
 
         ans = []
-
         for char, index in zip(queryCharacters, queryIndices):
-            update(
-                1,
-                0,
-                n - 1,
-                index,
-                char
-            )
-
+            if arr[index] != char:
+                arr[index] = char
+                update(1, 0, n - 1, index, char)
             ans.append(tree[1][4])
 
         return ans
