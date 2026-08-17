e4028bfa3c2df87c83912f2e2c21ee288f4ffb4f
Anish Singh <anishsingh.70455@gmail.com>
2026-08-17 18:44:06 +0530

    Time: 688 ms (73.33%), Space: 29.2 MB (81.18%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.py
+++ 1563-stone-game-v/1563-stone-game-v.py
@@ -2,73 +2,46 @@ class Solution:
     def stoneGameV(self, stoneValue):
         n = len(stoneValue)
 
-        if n == 1:
-            return 0
+        prefix = [0]
 
-        prefix = [0] * (n + 1)
+        for v in stoneValue:
+            prefix.append(prefix[-1] + v)
 
-        for i in range(n):
-            prefix[i + 1] = prefix[i] + stoneValue[i]
-
+        mid = list(range(n))
         dp = [[0] * n for _ in range(n)]
 
-        row_best = [[float('-inf')] * n for _ in range(n)]
-        col_best = [[float('-inf')] * n for _ in range(n)]
-
         for i in range(n):
-            row_best[i][i] = prefix[i + 1]
-            col_best[i][i] = -prefix[i]
+            dp[i][i] = stoneValue[i]
 
-        for length in range(2, n + 1):
-            for i in range(n - length + 1):
-                j = i + length - 1
+        max_score = 0
 
-                total = prefix[j + 1] - prefix[i]
+        for l in range(2, n + 1):
+            for i in range(n - l + 1):
+                j = i + l - 1
 
-                lo = i
-                hi = j - 1
+                while prefix[mid[i]] - prefix[i] < prefix[j + 1] - prefix[mid[i]]:
+                    mid[i] += 1
 
-                while lo <= hi:
-                    mid = (lo + hi) // 2
+                p = mid[i]
+                max_score = 0
 
-                    left = prefix[mid + 1] - prefix[i]
-                    right = total - left
+                if prefix[p] - prefix[i] == prefix[j + 1] - prefix[p]:
+                    max_score = max(dp[i][p - 1], dp[j][p])
+                else:
+                    if i <= p - 2:
+                        max_score = max(max_score, dp[i][p - 2])
 
-                    if left <= right:
-                        lo = mid + 1
-                    else:
-                        hi = mid - 1
+                    if p <= j:
+                        max_score = max(max_score, dp[j][p])
 
-                best = 0
-
-                if hi >= i:
-                    best = max(best, row_best[i][hi] - prefix[i])
-
-                if lo <= j - 1:
-                    best = max(best, col_best[lo + 1][j] + prefix[j + 1])
-
-                if hi >= i and lo == hi + 1:
-                    k = hi
-                    left = prefix[k + 1] - prefix[i]
-                    right = prefix[j + 1] - prefix[k + 1]
-
-                    if left == right:
-                        best = max(
-                            best,
-                            left + dp[i][k],
-                            right + dp[k + 1][j]
-                        )
-
-                dp[i][j] = best
-
-                row_best[i][j] = max(
-                    row_best[i][j - 1],
-                    prefix[j + 1] + dp[i][j]
+                dp[i][j] = max(
+                    dp[i][j - 1],
+                    prefix[j + 1] - prefix[i] + max_score
                 )
 
-                col_best[i][j] = max(
-                    col_best[i + 1][j],
-                    dp[i][j] - prefix[i]
+                dp[j][i] = max(
+                    dp[j][i + 1],
+                    prefix[j + 1] - prefix[i] + max_score
                 )
 
-        return dp[0][n - 1]
+        return max_score
